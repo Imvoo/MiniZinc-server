e3c0f020c1688f16af279c3c9cde1b0a031d6319
Sam Wright <sjwri7@student.monash.edu>
2016-08-26 19:25:56 +1000

    JSON Array per line

--- server.py
+++ server.py
@@ -20,13 +20,11 @@ def Solve(solver):
 			output = []
 			with Popen(["MiniZinc", solver+".mzn", "-a"], stdout=PIPE, bufsize=1, universal_newlines=True) as p:
 				for line in p.stdout:
-					if line.rstrip() == '----------':
-						yield '<br />'
-					else:	
-						yield line.rstrip()
+					if line.rstrip() != '----------':
+						yield json.dumps(line.rstrip())
 					#print(line, end='')
 		#output = pymzn.minizinc("" + solver + ".mzn")#, data={'n':4})
 		#return json.jsonify(output)
-		return Response(output_line(),  mimetype='text/html')
+		return Response(output_line(),  mimetype='text/json')
 	else:
 		return json.jsonify(solver="no solver found")
