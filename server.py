e84d9a59c6ca3c4f506f977dc46bf58dc4f50ea2
Sam Wright <sjwri7@student.monash.edu>
2016-09-05 00:46:21 +1000

    Attempting to use sockets to communicate between web page and Python backend
    Can’t get custom message types to work, something to work on..

--- server.py
+++ server.py
@@ -1,36 +1,20 @@
+#imports
 import pymzn
 import os
 from subprocess import Popen, PIPE, STDOUT
-from flask import Flask, json, Response, request
-app = Flask(__name__)
+from flask import Flask, json, Response, request, render_template
+from flask_socketio import SocketIO, emit
 
-from flask import Flask, render_template
-from flask_socketio import SocketIO
-
-
-#SOCKETS
-
+#setup
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+
+#sockets
 socketio = SocketIO(app)
-
-if __name__ == '__main__':
-    socketio.run(app)
+  
     
-@app.route("/")
-def index():
-    return render_template('index.html',)
 
-@socketio.on('send_message')
-def handle_source(json_data):
-    text = json_data['message'].encode('ascii', 'ignore')
-    socketio.emit('echo', {'echo': 'Server Says: '+text})
-
-if __name__ == "__main__":
-    socketio.run(app)
-
-##END SOCKETS
-    
+#REST    
 folder = 'models' #where the .mzn files are stored
 models = []
 for file in os.listdir(folder):
@@ -55,7 +39,8 @@ def Model(model):
 				for line in p.stdout:
 					markup = ['----------','==========']
 					if line.rstrip() not in markup: #each new solution is a new JSON object
-						yield str(pymzn.parse_dzn(line)).replace('\'', '\"') #use pymzn to turn output into nice JSON objects
+						solution = str(pymzn.parse_dzn(line)).replace('\'', '\"') #use pymzn to turn output into nice JSON objects
+						yield solution
 		return Response(output_line(),  mimetype='text/json')
 	else:
 		return json.jsonify(model="no model found")
@@ -68,3 +53,23 @@ def after_request(response):
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   return response
+
+
+
+@app.route('/stream')
+def stream():
+	return render_template('index.html')
+ 
+#why doesn't this run?   
+@socketio.on('request template')
+def request_template(model):
+	with Popen(["minizinc", folder + '/' + model+".mzn", "-a", "-D"], stdout=PIPE, stderr=STDOUT, bufsize=1, universal_newlines=True) as p: #-a outputs all solutions
+		for line in p.stdout:
+			markup = ['----------','==========']
+			if line.rstrip() not in markup: #each new solution is a new JSON object
+				solution = str(pymzn.parse_dzn(line)).replace('\'', '\"') #use pymzn to turn output into nice JSON objects
+				emit('solution', solution, broadcast=True)
+				
+#run
+if __name__ == '__main__':
+	socketio.run(app)
